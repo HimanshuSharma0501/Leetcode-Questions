6ecf6ec0d5be01a060ab73e8a71bc711156148cf
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-06-22 16:54:13 +0530

    2634 Filter elements from array JS

---
+++ 2634_Filter_Elements_from_array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  const filteredArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) filteredArray.push(arr[i]);
+  }
+  return filteredArray;
+};
