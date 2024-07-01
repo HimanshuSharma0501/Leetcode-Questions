ed98934d5b9a6c25a429b40742748672bab37a45
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-07-01 11:04:00 +0530

    1550 Threee consecutive odds

---
+++ 1550_ThreeConsecutiveOdds.py
@@ -0,0 +1,7 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        n=len(arr)
+        for i in range(n):
+            if i+2<n and arr[i]%2==1 and arr[i+1]%2==1 and arr[i+2]%2==1:
+                return True
+        return False        
