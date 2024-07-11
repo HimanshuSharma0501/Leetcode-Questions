5bfbcbb8a3bcf4742e9176799ed7efc86504c0cc
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-07-11 09:20:02 +0530

    1190 Reverse Substrings between each pair of parenthisis

---
+++ 1190_ReverseSubstringsBwEachPairOfParenthisis.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def __init__(self):
+        self.i = 0
+
+    def reverseParentheses(self, s):
+        def helper(s):
+            sb = []
+
+            while self.i < len(s):
+                if s[self.i] == ')':
+                    self.i += 1
+                    return ''.join(sb)[::-1]
+                elif s[self.i] == '(':
+                    self.i += 1
+                    st = helper(s)
+                    sb.append(st)
+                else:
+                    sb.append(s[self.i])
+                    self.i += 1
+
+            return ''.join(sb)
+
+        return helper(s)
+        
