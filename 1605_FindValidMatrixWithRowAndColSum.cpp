8f773191ff5a50d78f825b746df62d0cdc6d753a
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-07-20 09:58:59 +0530

    1605 Find Valid Matrix with row and column sum

---
+++ 1605_FindValidMatrixWithRowAndColSum.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<vector<int>> restoreMatrix(vector<int> &rowSum, vector<int> &colSum)
+    {
+        int n = rowSum.size();
+        int m = colSum.size();
+        vector<vector<int>> v(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int x = min(rowSum[i], colSum[j]);
+                v[i][j] = x;
+                rowSum[i] -= x;
+                colSum[j] -= x;
+            }
+        }
+        return v;
+    }
+};
