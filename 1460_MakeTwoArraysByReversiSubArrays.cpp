bf15dc0551a9ca3f0a3d687cabc9b0fbfed48e24
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-08-03 08:12:55 +0530

    1460 Make two arrays by reversing sub arrays updated

--- 1460_MakeTwoArraysByReversiSubArrays.cpp
+++ 1460_MakeTwoArraysByReversiSubArrays.cpp
@@ -3,25 +3,23 @@ class Solution
 public:
     bool canBeEqual(vector<int> &target, vector<int> &arr)
     {
-        if (target.size() == arr.size())
-        {
-            vector<int> elementCounts(1001, 0);
-            for (int i = 0; i < target.size(); i++)
-            {
-                elementCounts[target[i]]++;
-                elementCounts[arr[i]]--;
-            }
-            for (int count : elementCounts)
-            {
-                if (count != 0)
-                {
-                    return false;
-                }
-            }
-        }
-        else
+        if (target.size() != arr.size())
         {
             return false;
         }
+        vector<int> elementCounts(1001, 0);
+        for (int i = 0; i < target.size(); i++)
+        {
+            elementCounts[target[i]]++;
+            elementCounts[arr[i]]--;
+        }
+        for (int count : elementCounts)
+        {
+            if (count != 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
